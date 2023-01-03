1cfc1c94a9602ff58bf7d20fcea6022a79b3ee50
Chryfi <71967555+Chryfi@users.noreply.github.com>
2023-01-03 16:41:13 +0100

    Add round method to MathUtils

--- api/src/main/java/de/appdev2/utils/MathUtils.java
+++ api/src/main/java/de/appdev2/utils/MathUtils.java
@@ -1,5 +1,8 @@
 package main.java.de.appdev2.utils;
 
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+
 public class MathUtils {
     public static double randomRange(double min, double max) {
         return Math.random() * (max - min) + min;
@@ -7,5 +10,14 @@ public class MathUtils {
 
     public static int randomRange(int min, int max) {
         return (int) Math.round(Math.random() * (max - min) + min);
+    }
+
+    public static double round(double num, int digit) {
+        double digits = Math.pow(10, digit);
+        return (double) Math.round(num * digits) / digits;
+    }
+
+    public static float round(float num, int digit) {
+        return (float) round((double) num, digit);
     }
 }
